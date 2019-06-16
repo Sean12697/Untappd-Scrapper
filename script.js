6e581fdf46106711dc15cb6fb9ae2d3002ed15fc
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-06-16 17:25:17 +0100

    Beer Basic Details

--- script.js
+++ script.js
@@ -19,13 +19,21 @@ async function getBeers(url) {
             })
             .catch(err => {
                 //handle error
+                console.log("Error", err)
             });
     });
 }
 
 function beerJSON(beerHTML) {
     return {
-        name: $('.name a', beerHTML).text()
+        name: $('.name a', beerHTML).text(),
+        brewery: $('.brewery a', beerHTML).text(),
+        style: $('.style', beerHTML).text(),
+        their_rating: parseFloat($('.ratings .you:first-child p', beerHTML).text().replace("Their Rating (", "").replace(")", "")),
+        global_rating: parseFloat($('.ratings .you:last-child p', beerHTML).text().replace("Global Rating (", "").replace(")", "")),
+        abv: parseFloat($('.abv', beerHTML).text().replace("% ABV", "")),
+        ibu: parseFloat($('.ibu', beerHTML).text().replace(" IBU", "")),
+        last_checkin_url: 'https://untappd.com' + $('.details p:nth-child(4) a', beerHTML).attr().href
     }
 }
 
